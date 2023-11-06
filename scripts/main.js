ddd7a274026745286b59bbbb23c40fa9171c2480
thyly kanku <thylycingombe@icloud.com>
2023-11-05 18:55:29 -0600

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    const nav= document.querySelector("nav"); //Get first and only nav element object
+    nav.innerHTML= `
+<a href="index.html">HOME</a>
+<a href="activities.html">Activities</a>
+<a href="hike.html">Hikes</a>
+<a href="video.html">Video</a>
+    `;
+
+
+
+});//END DMcontentLoaded
